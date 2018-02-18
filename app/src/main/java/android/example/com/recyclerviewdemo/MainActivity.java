fe6a6218c7184110325ee3c55d68a1d35ec681a0
fanharilardian <fanharilfanharil@gmail.com>
2018-02-18 09:01:15 +0700

    Commit Kedua 1. Build Gradle a. compile 'com.android.support:recyclerview-v7:23.0.+'
    2. activity_main.xml
    a. Menambahkan <android.support.v7.widget.RecyclerView
    
    3. Membuat RecyclerAdapter.java
    a. extends RecyclerView.Adapter
    
    DONE!

--- app/src/main/java/android/example/com/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/android/example/com/recyclerviewdemo/MainActivity.java
@@ -2,12 +2,63 @@ package android.example.com.recyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    String[] country = {
+            "India",
+            "Japan",
+            "Kazakhstan",
+            "Greece",
+            "Colombia",
+            "Canada",
+            "Austria",
+            "France",
+            "Jamaica",
+            "Kenya",
+            "Libya",
+            "Kuwait",
+            "Jordan",
+            "Switzerland",
+            "Pilippines",
+            "Pakistan"
+    };
+
+    String[] capital = {
+            "New Delhi",
+            "Tokyo",
+            "Astana",
+            "Athens",
+            "Bogota",
+            "Ottawa",
+            "Vienna",
+            "Paris",
+            "Kingston",
+            "Nairobi",
+            "Tripoli",
+            "Kuwait City",
+            "Amman",
+            "Bern",
+            "Manila",
+            "Islamabad",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        adapter = new RecyclerAdapter(country,capital);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/android/example/com/recyclerviewdemo/RecyclerAdapter.java
+++ app/src/main/java/android/example/com/recyclerviewdemo/RecyclerAdapter.java
@@ -1,13 +1,16 @@
 package android.example.com.recyclerviewdemo;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Fanharil Ardian on 18/02/2018.
  */
 
-public class RecyclerAdapter extends RecyclerView.Adapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
     String[] country_names, country_capitals;
 
     public RecyclerAdapter(String[] country_names, String[] country_capitals) {
@@ -16,17 +19,30 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,parent,false);
+
+        RecyclerViewHolder recyclerViewHolder = new RecyclerViewHolder(view);
+        return recyclerViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        holder.Tx_Country.setText(country_names[position]);
+        holder.Tx_Capital.setText(country_capitals[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return country_names.length;
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView Tx_Country, Tx_Capital;
+        public RecyclerViewHolder(View view) {
+            super(view);
+            Tx_Country = (TextView) view.findViewById(R.id.tx_country_name);
+            Tx_Capital = (TextView) view.findViewById(R.id.tx_capital_name);
+        }
     }
 }
